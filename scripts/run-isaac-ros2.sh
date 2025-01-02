9d076eb2ffda43aaf591f3ea2e33402ed0d51839
maphysart <maphysart@gmail.com>
2025-01-02 21:06:34 +0800

    change to latest isaac version

--- scripts/run-isaac-ros2.sh
+++ scripts/run-isaac-ros2.sh
@@ -1,7 +1,7 @@
 # Runs Isaac Sim with its built-in ROS2 support
 
 # TODO set this variable
-export ISAAC_PATH=$HOME/.local/share/ov/pkg/isaac_sim-2023.1.1
+export ISAAC_PATH=$HOME/OSS/omniverse/pkg/isaac-sim-4.2.0
 
 if ! [[ -d $ISAAC_PATH ]]
 then
@@ -9,6 +9,9 @@ then
     echo "Please edit this script with the correct path."
     exit 1
 fi
+
+# Set ROS_DISTRO environment variable
+export ROS_DISTRO=humble  # Make sure this matches your ROS2 distro
 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ISAAC_PATH/exts/omni.isaac.ros2_bridge/humble/lib
